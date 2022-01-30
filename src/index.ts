5d298f16defb3c836f4396597a423ec40d6f78ad
SimonBucko <bucino.36@gmail.com>
2022-01-30 15:55:54 +0100

    added first routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express from "express"
+import authRoutes from "./routes/auth"
+
 const app = express()
+//allows utilize JSON body in request
+app.use(express.json())
+app.use("/auth", authRoutes)
+
 app.listen(8080,()=>{
     console.log("Server is runnig on port 8080")
 })
