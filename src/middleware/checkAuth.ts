e5162aa1658c5dd05aca4de00dc60844d14a3d6e
SimonBucko <bucino.36@gmail.com>
2022-01-31 22:50:27 +0100

    implemented middleware

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -14,4 +14,18 @@ export const checkAuth = async (req:Request,res:Response,next:NextFunction) => {
     }
 
     token = token.split(" ")[1]
+
+    try {
+        const user = (await JWT.verify(token,process.env.JWT_SECRET as string)) as {email:string}
+        req.user = user.email
+        next()
+    } catch (error) {
+        return res.status(403).json({
+            errors:[
+                {
+                    msg: "Unauthorized"
+                }
+            ]
+        })
+    }
 } 
