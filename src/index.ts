8d978d47c0fd210bc8e1c27e28fd5db4e2d3688f
SimonBucko <bucino.36@gmail.com>
2022-01-31 10:35:15 +0100

    added cors support

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express"
 import authRoutes from "./routes/auth"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cors from "cors"
 
 const app = express()
 dotenv.config()
 
 //allows utilize JSON body in request
 app.use(express.json())
+app.use(cors())
 app.use("/auth", authRoutes)
 
 mongoose.connect(process.env.MONGO_URI as string)
