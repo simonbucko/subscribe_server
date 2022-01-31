dfb6c4c3c3b2aea9f775c7f524a2827217056c9a
SimonBucko <bucino.36@gmail.com>
2022-01-31 08:11:32 +0100

    added signup route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import {body,validationResult} from "express-validator"
 
 const router = express.Router()
 
-router.post("/signup", async (req,res) => {
+router.post("/signup", body("email").isEmail().withMessage("The email is invalid") , body("password").isLength({min:5}).withMessage("The password should have at least 5 characters"),async (req,res) => {
+    //returns array of errors
+    const validationsErrors = validationResult(req)
+    if(!validationsErrors.isEmpty()){
+        const errors = validationsErrors.array().map(error => ({
+            msg: error.msg
+        }));
+        res.json({errors})
+    }
     const {email , password} = req.body;
     res.json({email,password})
 
