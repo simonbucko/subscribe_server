e944bce3fef8f4821c962362dde90637d4b1b74b
SimonBucko <bucino.36@gmail.com>
2022-01-31 09:17:58 +0100

    working on hashing pswd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from "express"
 import authRoutes from "./routes/auth"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+
 //allows utilize JSON body in request
 app.use(express.json())
 app.use("/auth", authRoutes)
 
-app.listen(8080,()=>{
-    console.log("Server is runnig on port 8080")
-})
+mongoose.connect(process.env.MONGO_URI as string)
+    .then(()=>{
+        console.log("Connected to DB")
+        app.listen(8080,()=>{
+            console.log("Server is runnig on port 8080")
+        })
+    })
+    .catch(() => {
+        throw new Error()
+    })
+
