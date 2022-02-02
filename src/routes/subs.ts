1cacab777ca24e8587b25f81a75c00f2370dcf60
SimonBucko <bucino.36@gmail.com>
2022-02-02 19:30:00 +0100

    implemented stripe integration

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import User from "../models/user"
 import { checkAuth } from "../middleware/checkAuth";
+import {stripe} from "../utils/stripe"
 
 
 const router = express.Router()
 
 router.get(`/prices`, checkAuth,async (req,res) => {
-    return res.send("prices")
+    const prices = await stripe.prices.list({
+        apiKey: process.env.STRIPE_SECRET_KEY
+    })
+
+    return res.json(prices)
 })
 
 export default router
